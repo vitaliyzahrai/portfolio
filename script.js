1b3a3d295c05f340092500e8ce7751d66770c82c
vitaliyzahrai <vitaliy.zahrai@gmail.com>
2021-01-10 03:38:11 +0200

    section skill

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ $(document).ready(function(){
     $('body').toggleClass('lock');
   });
 });
+
+$('.skills_per').each(function(){
+	var $this = $(this);
+	var per = $this.attr('per');
+	$this.css("width",per+'%');
+	$({animatedValue: 0}).animate({animatedValue: per},{
+		duration: 1000,
+		step: function(){
+			$this.attr('per', Math.floor(this.animatedValue) + '%');
+		},
+		complete: function(){
+			$this.attr('per', Math.floor(this.animatedValue) + '%');
+		},
+	});
+});
